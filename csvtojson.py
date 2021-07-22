71382cf6edc20bce13ece2af7cb7570c5425aca1
Rachel <rachelauslander@college.harvard.edu>
2021-07-22 16:49:55 -0400

    updated

--- csvtojson.py
+++ csvtojson.py
@@ -1,11 +1,11 @@
-import csv
+from csv import DictReader
 import json
-
-csv_file = open("citingslaveryjuly19.csv", "r")
-json_file = open("citingslavery.json", "w")
-
-citing_slavery = csv.DictReader(csv_file)
-
-for row in citing_slavery:
-    json.dump(row, json_file)
-    json_file.write("\n")
+csvfile = open("citingslaveryjuly19.csv", "r")
+output = {}
+processed_data = DictReader(csvfile)
+for row in processed_data:
+    output[row['cap_id']] = row
+csvfile.close()
+jsonfile = open("citingslavery.json", "w")
+json.dump(output, jsonfile, indent=2)
+jsonfile.close()
