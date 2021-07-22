5026a36c37cb8e66bffaa648f1b2f519dcd0b1d3
Rachel <rachelauslander@college.harvard.edu>
2021-07-22 17:00:57 -0400

    deleted extra cap id row

--- csvtojson.py
+++ csvtojson.py
@@ -5,6 +5,8 @@ output = {}
 processed_data = DictReader(csvfile)
 for row in processed_data:
     output[row['cap_id']] = row
+    row.pop('cap_id')
+
 csvfile.close()
 jsonfile = open("citingslavery.json", "w")
 json.dump(output, jsonfile, indent=2)
